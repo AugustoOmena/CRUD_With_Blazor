07f644d209a24a67d78a34685eebbc36136c34ed
Augusto Omena <augusto.omena.n@gmail.com>
2024-02-16 20:21:35 -0300

    Adicionar arquivos de projeto.

---
+++ BlazorApp1/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BlazorApp1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorApp1.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; } = null!;  
+    }
+}
